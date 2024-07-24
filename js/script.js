731c47058d2ca9257929a333d8cc999285cd11e3
Emanuel Pages <emapages334@gmail.com>
2024-07-23 21:08:53 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let nombre = prompt('INSERTE UN NOMBRE');
+let edad = parseInt(prompt('INGRESE UNA EDAD'));
+
+if(edad<18){
+    alert('ESTE SITIO NO PARA TI');
+}else if(edad=>18){
+    prompt('tu entrada vale: 2000');
+}
+console.log(nombre);
+console.log(edad);
