f9693cac804785f59521be699fd9b1e8417dcb55
Emanuel Pages <emapages334@gmail.com>
2024-08-06 19:17:21 -0300

    arrays puiestos

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 let nombre = prompt('INSERTE UN NOMBRE');
 let edad = parseInt(prompt('INGRESE UNA EDAD'));
 
+const edad1 = '18';
+const edad2 = '17';
+const edad3 = '19';
+
+const edades =['18', '17', '19']
 if(edad<18){
-    alert('ESTE SITIO NO PARA TI');
+    alert('ESTE SITIO NO ES PARA TI');
 }else if(edad=>18){
     prompt('tu entrada vale: 2000');
 }
 console.log(nombre);
 console.log(edad);
+console.log(edades[0]);
